f3801fbd88be20912ceb5fc58e7d51731b78e36e
AltF4 <altf4@phx2600.org>
2013-04-20 13:49:23 -0700

    + ‘prove_it/gather.py’ + ‘prove_it/hashes’

---
+++ prove_it/gather.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import socket
+import time
+
+for i in range(0, 1000):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("174.129.103.33", 9001))
+
+	s.recv(1024)
+	s.recv(95)
+
+	inString = s.recv(92)
+
+	inString = inString[12:25]
+
+	print inString
+
+	inString += "\n"
+
+	with open("hashes", "a") as myfile:
+		myfile.write(inString)
+
+
+
